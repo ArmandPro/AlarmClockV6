5951fb89a3c89c579c23d6905c263fec5f5385d8
Eibu <eibuxxiv@gmail.com>
2017-11-12 15:20:45 -0500

    ScrollingActivity update : check management

--- app/src/main/java/com/example/etudes/alarmclockv6/MainActivity.java
+++ app/src/main/java/com/example/etudes/alarmclockv6/MainActivity.java
@@ -1,31 +1,30 @@
 package com.example.etudes.alarmclockv6;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.app.AlertDialog.Builder;
 
 import com.example.etudes.alarmclockv6.Database.DatabaseManager;
+import com.example.etudes.alarmclockv6.MiniGames.MatrixGame;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.facebook.login.LoginManager;
 
 import java.util.Calendar;
 
@@ -135,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent nIntent = new Intent(MainActivity.this, ScrollingActivity.class);
+                Intent nIntent = new Intent(MainActivity.this, MatrixGame.class);
                 startActivity(nIntent);
 
             }
